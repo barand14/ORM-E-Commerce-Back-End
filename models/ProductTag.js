b5e0c3cecc6e04a6e44e9723baa7c7a3b518eb51
Balalomba Barandao <barand14@gmail.com>
2024-05-29 10:21:01 -0400

    edited product-tag.js, index.js, category-routes.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,29 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      autoIncrement: true, //Serial
+      allowNull: false, //required
+      primaryKey: true // is the primary key/id of each entry on this table
+    },
+    
+    product_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references:{
+        model:"product",
+        key: "id"
+      }
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references:{
+        model:"tag",
+        key: "id"
+      }
+    },
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,65 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { update } = require('../../models/Product');
 
-// The `/api/categories` endpoint
+// The `localhost:3001/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include:[Product]
+  })
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(400).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, { include: [Product] })
+    .then(data => {
+      if (!data) {
+        return res.status(404).json({ message: 'Category not found' });
+      }
+      res.status(200).json(data);
+    })
+    .catch(err => res.status(400).json(err));
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(data => res.status(200).json({ message: 'Category created successfully!', data }))
+  .catch(err => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: { id: req.params.id }
+  })
+    .then(() => Category.findByPk(req.params.id)) // Find updated category
+    .then(updatedCategory => {
+      if (!updatedCategory) {
+        return res.status(404).json({ message: 'Category not found' });
+      }
+      res.status(200).json(updatedCategory);
+    })
+    .catch(err => res.status(400).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({ where: { id: req.params.id } })
+  .then(data => { // data will be the number of rows deleted
+    if (!data) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+    res.status(200).json({ message: 'Category deleted successfully!' });
+  })
+  .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
